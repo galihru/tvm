0f2aa61e3343f0cece82ae625b3cfa0ec2144fc0
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-06-22 07:53:52 +0700

    Update lr_suggester.py

--- src/lr_suggester.py
+++ src/lr_suggester.py
@@ -1,8 +1,9 @@
-import math, numpy as np
+import math
+import numpy as np
 
-def suggest_lr(complexity: float, epochs: int=50, grad_var: float=None):
+def suggest_lr(complexity: float, epochs: int = 50, grad_var: float = None):
     if grad_var:
-        lr = math.sqrt(2/(epochs*grad_var))
+        lr = math.sqrt(2 / (epochs * grad_var))
     else:
-        lr = 1/math.sqrt(max(complexity,1))
+        lr = 1 / math.sqrt(max(complexity, 1))
     return float(np.clip(lr, 1e-5, 1e-1))
