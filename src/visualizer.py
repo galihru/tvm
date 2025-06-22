55cd8ea1feafb7b9d97a2da172ff074b93cde2a8
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-06-22 07:55:34 +0700

    Update visualizer.py

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,16 +1,23 @@
 import matplotlib.pyplot as plt
 
-class Visualizer:
-    """
-    Plot loss curves and metrics.
-    """
-    @staticmethod
-    def plot_loss(history: dict):
-        plt.figure()
-        plt.plot(history['train_loss'], label='Train Loss')
-        plt.plot(history['val_loss'], label='Val Loss')
-        plt.legend()
-        plt.xlabel('Epoch')
-        plt.ylabel('MSE Loss')
-        plt.title('Training vs Validation Loss')
-        plt.show()
+
+def plot_losses(train_loss, val_loss):
+    plt.figure()
+    plt.plot(train_loss, label='Train Loss')
+    plt.plot(val_loss, label='Val Loss')
+    plt.legend(); plt.xlabel('Epoch'); plt.ylabel('Loss'); plt.title('Loss Curve')
+    plt.show()
+
+
+def plot_metrics(metrics: dict):
+    labels = list(metrics.keys())
+    values = list(metrics.values())
+    plt.figure()
+    plt.bar(labels, values)
+    plt.title('Model Metrics')
+    plt.show()
+
+
+def plot_complexity(report: dict):
+    # visualisasi laporan kompleksitas tergantung tipe
+    pass
