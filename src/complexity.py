8012e2d19491e232937b8161725733f421b71734
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-06-22 07:53:09 +0700

    Update complexity.py

--- src/complexity.py
+++ src/complexity.py
@@ -1,11 +1,12 @@
 import math
-import numpy as np
 
 def tabular_complexity(n_samples, n_features, avg_corr):
-    return math.log1p(n_features) * (1-avg_corr) * math.sqrt(n_samples)
+    return math.log1p(n_features) * (1 - avg_corr) * math.sqrt(n_samples)
+
 
 def image_complexity(avg_w, avg_h, class_count, imbalance):
-    return (avg_w*avg_h) * math.log1p(class_count) / (1+imbalance)
+    return (avg_w * avg_h) * math.log1p(class_count) / (1 + imbalance)
+
 
 def text_complexity(entropy, vocab_size):
     return entropy * math.sqrt(vocab_size)
